919f76a385dee4667148ec2b59db695cf443edbd
Chris Vitalos <cvitalos@gmail.com>
2025-06-01 22:50:38 -0400

    added waitress as WSGI server for flask application

--- server/__main__.py
+++ server/__main__.py
@@ -1,5 +1,6 @@
 # __main__.py
-from app import app
+from server.app import app
+from waitress import serve
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    serve(app, host='127.0.0.1', port=8000, threads=4)
