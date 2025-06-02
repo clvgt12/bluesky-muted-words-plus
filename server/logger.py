e64f5c7c8888345aa6b16ef86a184310d32816ee
Chris Vitalos <cvitalos@gmail.com>
2025-06-01 22:51:35 -0400

    now honors FLASK_DEBUG environment variable setting in .env

--- server/logger.py
+++ server/logger.py
@@ -3,22 +3,21 @@
 import logging
 import os
 import sys
+from server.config import FLASK_DEBUG
 
 def setup_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.DEBUG if FLASK_DEBUG else logging.ERROR)
 
     if logger.hasHandlers():
         return logger  # Prevent adding handlers multiple times
 
-    debug_mode = os.getenv("FLASK_DEBUG", "0").lower() in ("1", "true", "yes")
-
-    if not debug_mode:
+    if not FLASK_DEBUG:
         logging.getLogger("werkzeug").setLevel(logging.ERROR)  # silence completely
         logging.getLogger("httpx").setLevel(logging.ERROR)  # silence completely
-        
+
     handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG if debug_mode else logging.ERROR)
+    handler.setLevel(logging.DEBUG if FLASK_DEBUG else logging.ERROR)
 
     formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
     handler.setFormatter(formatter)
