3fcd0842277c6808663037658e439593f2e1eecb
Chris Vitalos <cvitalos@gmail.com>
2025-06-07 20:58:41 -0400

    added command line parameters to support starting app from command line

--- server/__main__.py
+++ server/__main__.py
@@ -1,6 +1,30 @@
 # __main__.py
-from server.app import app
+import argparse
+import sys
+from server import config
+from server.app import app, start_data_stream_thread
 from waitress import serve
 
+def main():
+    # Define parser and arguments
+    parser = argparse.ArgumentParser(description="Start Bluesky Flask app with Waitress.")
+    parser.add_argument('--host', default=config.HOST, help=f"Hostname (default: {config.HOST})")
+    parser.add_argument('--port', type=int, default=int(config.PORT), help=f"Port (default: {config.PORT})")
+    parser.add_argument('--threads', type=int, default=int(config.THREADS), help=f"Thread count (default: {config.THREADS})")
+
+    # Check if user asked for help *before* parsing
+    if '--help' in sys.argv or '-h' in sys.argv:
+        parser.print_help()
+        sys.exit(1)
+
+    # Parse arguments normally
+    args = parser.parse_args()
+
+    # Start the background data stream consumer thread process
+    start_data_stream_thread()
+
+    # Start Waitress server
+    serve(app, host=args.host, port=args.port, threads=args.threads)
+
 if __name__ == '__main__':
-    serve(app, host='127.0.0.1', port=8000, threads=4)
+    main()
